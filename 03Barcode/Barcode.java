24e28f66fc2081557a86d7dc2de2bc2c7aa0f5b9
WhatABbas <aabbas@stuy.edu>
2016-12-11 20:23:11 -0500

    Done?

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 public class Barcode implements Comparable<Barcode>{
     private String _zip;
-    private static final String[] b4rz = {":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::", "||:::"};
+    private static final String[] b4rz = {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::"};
 
     public Barcode(String zip){
         IllegalArgumentException oops = new IllegalArgumentException("Make sure you are inputting a 5 digit string of numbers!");
@@ -17,7 +19,7 @@ public class Barcode implements Comparable<Barcode>{
     }
 
     public String toString(){
-        return _zip + " " + makeCode(_zip);
+        return _zip + checkSum(_zip) + " " + toCode(_zip);
     }
 
     private static String checkSum(String zip){
@@ -28,7 +30,7 @@ public class Barcode implements Comparable<Barcode>{
     	return "" + cSum % 10;
     }
 
-    public static String makeCode(String zip){
+    public static String toCode(String zip){
     	String bars = "|";
         zip = zip + checkSum(zip);
     	for(int i = 0; i < zip.length(); i++){
@@ -39,21 +41,36 @@ public class Barcode implements Comparable<Barcode>{
     	return bars;
     }
 
-    private boolean checkCode(String code){
-        if(code.length() < 32){
-            return false;
+    private static void checkCode(String code){
+        if((code.length() - 2) % 5 != 0){
+            throw new IllegalArgumentException("Your barcode must be a multiple of 5, + 2 characters long!");
         }
         if(code.charAt(0) != '|' || code.charAt(code.length() - 1) != '|'){
-            return false;
+            throw new IllegalArgumentException("There must be an initial and final pipe in the barcode!");
         }
         for(int i = 0; i < code.length(); i++){
             char letta = code.charAt(i);
             if(letta != ':' || letta != '|'){
-                return false;
+                throw new IllegalArgumentException("Barcodes can only contain '|'s or ':'s!");
             }
         }
-        return true;
+        for(int i = 1; i < code.length()-1; i+=5){
+            if(!(Arrays.asList(b4rz).contains(code.substring(i,i+5)))){
+                throw new IllegalArgumentException("Invalid bar values!");
+            }
+        }
+    }
 
+    public static String toZip(String code){
+        checkCode(code);
+        String zip = "";
+        for(int i = 1; i < code.length() -1; i+=5){
+            zip += "" + Arrays.asList(b4rz).indexOf(code.substring(i, i+5));
+        }
+        if((zip.substring(zip.length()-1, zip.length())) != checkSum(zip.substring(0, zip.length()-1))){
+            throw new IllegalArgumentException("Your Check Digit isn't valid!");
+        }
+        return zip.substring(0, zip.length()-1);
     }
 
     private static String intToBar(String i){

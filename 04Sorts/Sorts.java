f0586553c14c811b18fd842f5c908914766d65ca
Adam Abbas <aabbas@stuy.edu>
2016-12-14 06:36:44 -0500

    Added Insertion Sort

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
 public class Sorts{
     
     public static String name(){
 	return "10.Abbas.Adam";
     }
 
+    /**Selection sort of an int array. 
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
     public static void selectionSort(int[] data){
 	int nowMin, nowDex;
 	for(int strt = 0; strt < data.length-1; strt++){
@@ -20,13 +25,24 @@ public class Sorts{
 	}
     }
 
-     /**Selection sort of an int array. 
-  *Upon completion, the elements of the array will be in increasing order.
-  *@param data  the elements to be sorted.
-  */
+    public static void insertionSort(int[] data){
+	int bNMoved, cTDwn;
+	for(int i = 1; i < data.length; i++){
+	    bNMoved = data[i];
+	    cTDwn = i-1;
+	    while(cTDwn >= 0 && bNMoved < data[cTDwn]){
+		data[cTDwn + 1] = data[cTDwn];
+		cTDwn--;
+		System.out.println(Arrays.toString(data));
+	    }
+	    data[cTDwn+1] = bNMoved;
+	}
+    }
+
+     
     public static void main(String[] aragasdrtawelkjhaselkgasdklasdghkl){
 	int[] test1 = {8, 5, 6, 2, 7, 3, 1, 9, 0, 4};
-	selectionSort(test1);
+	insertionSort(test1);
 
 	for(int i: test1){
 	    System.out.println(test1[i]);
